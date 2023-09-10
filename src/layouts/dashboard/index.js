dfa940302facb28688aa782ed5da6687e2a2b975
Innovix <htetnainghein@innovix-solutions.com>
2023-09-10 22:32:00 +0630

    Nav Bar

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,11 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import AntSwitch from "../../components/AntSwitch";
+import useSettings from "../../hooks/useSettings";
 import { Outlet } from "react-router-dom";
+import { Nav_Buttons } from "../../data";
+import { faker } from "@faker-js/faker";
+
+import Logo from "../../assets/Images/logo.ico";
+import { Gear } from "phosphor-react";
 
 const DashboardLayout = () => {
+  const theme = useTheme();
+
+  const [selected, setSelected] = useState(0);
+
+  const { onToggleMode } = useSettings();
+
+  console.log(theme);
 
   return (
     <>
-      Dashboard Layout
+      <Box
+        p={2}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Stack
+          direction="column"
+          alignItems={"center"}
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
+          spacing={3}
+        >
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+            >
+              <img src={Logo} alt={"Chat App"} />
+            </Box>
+            <Stack
+              sx={{ width: "max-content" }}
+              direction="column"
+              alignItems={"center"}
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{ width: "max-content", color: theme.palette.mode === 'light' ? "#000" : theme.palette.text.primary }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
+                <Box
+                  p={1}
+                  sx={{
+                    backgroundColor: theme.palette.primary.main,
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
+                  </IconButton>
+                </Box>
+              ) : (
+                <IconButton
+                  onClick={() => {
+                    setSelected(3);
+                  }}
+                  sx={{ width: "max-content", color: theme.palette.mode === 'light' ? "#000" : theme.palette.text.primary }}
+                >
+                  <Gear />
+                </IconButton>
+              )}
+            </Stack>
+          </Stack>
+          <Stack spacing={4}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar()} />
+          </Stack>
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );

8dbfe50ebbb12e576e969b5c81a507d69662b987
Innovix <htetnainghein@innovix-solutions.com>
2023-09-24 03:15:23 +0630

    Login Form

---
+++ src/sections/auth/LoginForm.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider, { RHFTextField } from "../../components/hook-form";
+import { Alert, Button, IconButton, InputAdornment, Link, Stack } from "@mui/material";
+// import { LoadingButton } from "@mui/lab";
+import { Eye, EyeSlash } from "phosphor-react";
+// import { useDispatch, useSelector } from "react-redux";
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  // const {isLoading} = useSelector((state) => state.auth);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const defaultValues = {
+    email: "demo@talkspire.com",
+    password: "demo1234",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      // Submit data to backend
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+
+        <RHFTextField name="email" label="Email address" />
+
+        <RHFTextField
+          name="password"
+          label="Password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+      </Stack>
+
+      <Stack alignItems="flex-end" sx={{ my: 2 }}>
+        <Link component={RouterLink} to="/auth/reset-password" variant="body2" color="inherit" underline="always">
+          Forgot password?
+        </Link>
+      </Stack>
+
+      <Button
+        fullWidth
+        color="inherit"
+        size="large"
+        type="submit"
+        variant="contained"
+        // loading={isLoading}
+        sx={{
+          bgcolor: "text.primary",
+          color: (theme) =>
+            theme.palette.mode === "light" ? "common.white" : "grey.800",
+          "&:hover": {
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+          },
+        }}
+      >
+        Login
+      </Button>
+
+    </FormProvider>
+  );
+};
+
+export default LoginForm;
